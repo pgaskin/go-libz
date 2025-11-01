68afc32257da9b2473666a80843ff785f6a04978
Patrick Gaskin <patrick@pgaskin.net>
2025-11-01 14:49:30 -0400

    Implmement module instantiation

--- libz.go
+++ libz.go
@@ -53,3 +53,45 @@ func initialize() {
 
 	instance.compiled, instance.err = instance.runtime.CompileModule(ctx, bin)
 }
+
+type libz struct {
+	ctx context.Context
+	mod api.Module
+}
+
+func instantiate() (*libz, error) {
+	if err := Initialize(); err != nil {
+		return nil, err
+	}
+
+	z := new(libz)
+	z.ctx = context.Background()
+
+	// we don't need wasi_snapshot_preview1 since we optimized out the gzip stuff which requires filesystem functions
+
+	var err error
+	z.mod, err = instance.runtime.InstantiateModule(z.ctx, instance.compiled, wazero.NewModuleConfig().WithName(""))
+	if err != nil {
+		return nil, err
+	}
+	if z.getfn("zError") == nil {
+		return nil, errors.New("libz: bad wasm binary")
+	}
+	return z, nil
+}
+
+func (z *libz) getfn(name string) api.Function {
+	return z.mod.ExportedFunction(name)
+}
+
+// pool is a pool of instantiated sqlite modules.
+var pool sync.Pool
+
+// poolInstantiate gets a instantiated module from [presspool], or instantiates
+// a new one.
+func poolInstantiate() (*libz, error) {
+	if x := pool.Get(); x != nil {
+		return x.(*libz), nil
+	}
+	return instantiate()
+}
